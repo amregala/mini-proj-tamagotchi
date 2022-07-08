cc7a0472099c479f82c139cb150937abc9a2fab1
amregalado <regalado.adriana@gmail.com>
2022-07-08 17:32:46 -0400

    initial base class created

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 console.log("Tamagotch | Mini Project");
+
+// PET CLASS
+class Pet {
+  constructor(name, age, hunger, sleepiness, bordem) {
+    this.name = name;
+    this.health = this.hunger = 10;
+    this.sleepiness = 0;
+    this.bordem = 0;
+  }
+
+  //PET CLASS METHODS
+  //get name () - returns name
+  getName() {
+    return this.name;
+  }
+}
