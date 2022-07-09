8815c36ed832a6f092a6545df26533b766209915
amregalado <regalado.adriana@gmail.com>
2022-07-09 13:42:34 -0400

    "Dino ages up at intervals, animates across screen"

--- app.js
+++ app.js
@@ -27,8 +27,16 @@ class Pet {
   ageInit() {
     return this.age;
   }
+
   ageUp() {
-    this.age = 0;
+    let ageCount = 0;
+    const ageIncrement = document.querySelector(".age");
+    setInterval(() => {
+      ageCount += 1;
+      ageIncrement.innerHTML = `Age: ${ageCount}`;
+    }, 3000);
+    this.age = ageCount;
+    return this.age;
   }
 }
 
@@ -37,9 +45,11 @@ const dino = new Pet("Dino");
 dino.getName();
 //console.log(dino.getName());
 
+//start of game
 const start = document.querySelector(".startBtn");
 
 const gameStart = function nameInput() {
+  console.log("game started");
   userID = prompt("What is your Dino's name?");
   const user = document.querySelector("#userName");
   user.innerHTML = `Dino-${userID}`;
@@ -59,4 +69,62 @@ const gameStart = function nameInput() {
   const bored = document.querySelector(".bordemScore");
   bored.innerHTML = `Boredem ${dino.getBorded()}`;
 
+  dino.ageUp();
+};
+
 start.addEventListener("click", gameStart);
+
+//lightswitch
+function onOff() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+// seconds += 1;
+// this.age =+
+// ageIncrement.innerText =  `Age ${seconds}` ;
+
+//   start.addEventListener("click", () => {
+//     naming();
+//     let timer = setInterval(() => {
+//       tom.aging();
+//       if (
+//         dino.hunger >= 10 ||
+//         dino.hunger <= 0 ||
+//         dino.boredom >= 10 ||
+//         tom.boredom <= 0 ||
+//         tom.sleepy >= 10 ||
+//         tom.sleepy <= 0
+//       ) {
+//         console.log("Gameover");
+//         popScore.innerText = `Gameover. Your Tamagotchi has died!!`;
+//         clearInterval(timer);
+//       }
+//     }, 5000);
+//   });
+
+// ageUp() {
+//     this.age = 0;
+//     var minutesToAdd = 30;
+//     var currentDate = new Date();
+//     var futureDate = new Date(currentDate.getTime() + minutesToAdd * 60000);
+//   }
+
+//   aging() {
+//     this.age += 1;
+//     this.hunger += 1;
+//     this.sleep += 1;
+//     this.boredom += 1;
+//     console.log(this.hunger, this.age, this.sleep, this.boredom);
+//     ageScore.innerText = this.age;
+//     sleepScore.innerText = this.sleep;
+//     hungerScore.innerText = this.hunger;
+//     playScore.innerText = this.boredom;
+//   }
+
+//       const ageIncrement = document.querySelector(".age");
+//       let ageCount = this.age;
+//       ageCount += 1;
+//       ageIncrement.innerText = `Age ${ageCount}`;
+//     }, 10000);
+//
